db2c1eb83ca2cfdb382fb547fae43489e6255093
El Mehdi Bensouda <132353765+ElMehdi02@users.noreply.github.com>
2023-08-23 02:09:02 +0100

    Update shell.h

--- shell.h
+++ shell.h
@@ -1,88 +1,67 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef _shell_h
+#define _shell_h
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
-#include <stddef.h>
 #include <string.h>
-#include <errno.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
-#include <signal.h>
+#include <limits.h>
 #include <fcntl.h>
+#include <errno.h>
 
-#define READ_SIZE 1024
-#define BUFSIZE 1024
+/* read - write buffers */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+#define TRUE (1 == 1)
+
+/* my_path function*/
+int my_path(char *xfirst, char **inpt, char **env, int *exit_st);
+
+
+/* env function */
+int print_env(char **env, int *exit_st);
+char *cpy_env(char **env);
+
+/* built- execute functions */
+void built_ins(char *c_lin, char **args, char **env, int *exit_st);
+void exec_env(int status, char **args, int *exit_st, int *taly);
+
+/* strings  functions */
+int str_len(char *s);
+char *str_dup(char *strg);
+int str_cmp(char *str1, char *str2);
+char *str_cpy(char *dstn, char *sourc);
+char *str_cat(char *dstn, char *sourc);
+int put_char(char ch);
+void printstr_(char *st);
+char *path_str(char *p_right, char *xfirst);
+char **_parser(char *l);
+void print_int(int *taly);
+
+
+/* o r functions */
+void no_nul(char *l);
+void grid_free(char **gridd, int _height);
+int char_special(char *buffer, ssize_t byt, int *exit_st);
+
+ssize_t _getline(char **line, size_t *n, FILE *stream);
+char *str_tok(char *str, const char *delimiters);
+void *_realloc(void *ptr, size_t size);
+void *_memcpy(void *destination, const void *src, size_t size);
+int _getsize(const char *str);
+void _puts(const char *str);
+int _putchar(char c);
+
+/*setenv unsetenv*/
+void _setenv(char **arr);
+void _unsetenv(char **arr);
+char *get_env(const char *glob_v);
+char *concatenat(char *glob_v, char *valeur1, char *valeur);
 
 extern char **environ;
 
-/* get_line.c */
-ssize_t _getline(char **buffer, size_t *bufsize, int fd);
-
-/* handle_input.c */
-int handle_input(char *buf, char **env, char **av);
-void sigint_handler(int sigint);
-char **tokenizez(char *str, char *delim);
-
-/* split_string.c */
-char **tokenize(char *buf);
-
-/* handle_builtins.c */
-void print_env(char **env);
-int builtins(char **tokens, char **env);
-int handle_builtins2(char **tokens);
-int handle_builtins3(char **tokens);
-int handle_builtins1(char **tokens, char **env);
-
-/* functions1.c */
-int _strlen(char *str);
-int _strcmp(char *s1, char *s2);
-int _strncmp(char *str1, char *str2, int n);
-char *_strdup(char *str);
-char *_strcatpath(char *dest, char *src);
-
-/* functions2.c */
-int _atoi(char *str);
-char *_strcpy(char *dest, char *src);
-char *_itoa(int num);
-int _constcharlen(const char *str);
-void print_error(char *av, int count, char *cmd, char *msg);
-
-/* functions3.c */
-char *_strchr(char *s, char c);
-char *_strtok(char *str, char *delim);
-char *_strcat(char *dest, char *src);
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
-/* free_array.c */
-void free_array(char **array);
-
-/* check_cmp.c */
-int check_cmd(char *cmd);
-
-/* get_path.c */
-char *get_path(char *cmd);
-char *make_path(char *path, char *cmd);
-
-/* run_cmd.c */
-int run_cmd(char **tokens, char **env, char **av);
-void pr_error(char **tokens, char **av);
-int fork_run(char **tokens, char **env);
-
-/* edit_env.c */
-int len_env(char **env);
-char *_getenv(char *key);
-
-/* my_cd.c */
-void my_cd(char **args);
-void change_directory(char **directories, char *command_name);
-
-/* handle_echo.c */
-int handle_echo(char **tokens, int *status);
-void handle_echo_args(char *tokens, int *status);
-char *get__env(char *key);
-
-char *cut_string(char *str);
-#endif /* SHELL_H */
+#endif
